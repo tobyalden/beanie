93843d4195726b96dfeb4a603be5783e212ef387
Toby Alden <toby.alden@gmail.com>
2022-09-15 16:15:38 -0700

    Add flight

--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -13,12 +13,15 @@ class Player extends Entity
 {
     public static inline var SPEED = 175;
     public static inline var GRAVITY = 900;
-    public static inline var MAX_FALL_SPEED = 200;
+    public static inline var MAX_FALL_SPEED = 300;
+    public static inline var MAX_RISE_SPEED = 200;
     public static inline var JUMP_POWER = 200;
     public static inline var JUMP_CANCEL_POWER = 40;
+    public static inline var FLIGHT_POWER = 1800;
 
     private var sprite:Spritemap;
     private var velocity:Vector2;
+    private var canFly:Bool;
 
     public function new(x:Float, y:Float) {
         super(x, y);
@@ -27,6 +30,7 @@ class Player extends Entity
         sprite.add("idle", [0]);
         graphic = sprite;
         velocity = new Vector2();
+        canFly = false;
     }
 
     override public function update() {
@@ -50,6 +54,7 @@ class Player extends Entity
             velocity.y = 0;
             if(Input.pressed("jump")) {
                 velocity.y = -JUMP_POWER;
+                canFly = false;
             }
         }
         else {
@@ -58,12 +63,23 @@ class Player extends Entity
                 gravity *= 0.5;
             }
             velocity.y += gravity * HXP.elapsed;
-            velocity.y = Math.min(velocity.y, MAX_FALL_SPEED);
-            if(Input.released("jump")) {
+            if(Input.check("jump") && canFly) {
+                velocity.y -= FLIGHT_POWER * HXP.elapsed;
+            }
+            else if(Input.released("jump")) {
+                canFly = true;
                 velocity.y = Math.max(velocity.y, -JUMP_CANCEL_POWER);
             }
+            velocity.y = MathUtil.clamp(velocity.y, -MAX_RISE_SPEED, MAX_FALL_SPEED);
         }
         moveBy(velocity.x * HXP.elapsed, velocity.y * HXP.elapsed, ["walls"]);
+    }
+
+    override public function moveCollideY(e:Entity) {
+        if(velocity.y < 0) {
+            velocity.y = -velocity.y;
+        }
+        return true;
     }
 
     private function animation() {
